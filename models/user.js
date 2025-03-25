3d2720a9961ab0b474230173cf32529bad8d90cc
Aditya7880900936 <adityasanskarsrivastav788@gmail.com>
2025-03-26 00:55:09 +0530

    userschema and hashing of password is done using crypto

---
+++ models/user.js
@@ -0,0 +1,56 @@
+const { createHmac, randomBytes } = require("crypto");
+
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    salt: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profileImageURL: {
+      type: String,
+      default: "/images/userAvatar.png",
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) return;
+
+  const salt = randomBytes(16).toString();
+  const hashedPassword = createHmac("hsa256", salt)
+    .update(user.password)
+    .digest("hex");
+    
+    this.salt = salt;
+    this.password = hashedPassword;
+    next(); 
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
