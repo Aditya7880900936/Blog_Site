a6669e698c260552a81a6865d51b18ea68a8b3c2
Aditya7880900936 <adityasanskarsrivastav788@gmail.com>
2025-03-29 16:15:49 +0530

    integrate cloudinary instead of multer for cloud storage of data

--- routes/blog.js
+++ routes/blog.js
@@ -1,59 +1,47 @@
 const { Router } = require("express");
-const multer = require("multer");
+const { upload } = require("../Config/cloudinaryconfig"); // Cloudinary config file
 const path = require("path");
 const router = Router();
 const Blog = require("../models/blog");
 const Comment = require("../models/comment");
 
+router.get("/add-new", (req, res) => {
+    return res.render("addBlog", {
+        user: req.user,
+    });
+});
 
-
-const storage = multer.diskStorage({
-    destination: function (req,file,cb){
-        cb(null, path.resolve(`./public/uploads/`));
-    },
-    filename: function (req,file,cb){
-       const fileName = `${Date.now()}-${file.originalname}`;
-         cb(null, fileName);
-    }
-})
-
-const upload = multer({storage: storage});
-
-
-router.get("/add-new",(req,res)=>{
-    return res.render("addBlog" , {
-        user : req.user,
-    })
-})
-
-router.get("/:id", async (req,res)=>{
+router.get("/:id", async (req, res) => {
     const blog = await Blog.findById(req.params.id).populate("createdBy");
-    const comments = await Comment.find({blogId: req.params.id}).populate("createdBy")
-    return res.render("blog",{
+    const comments = await Comment.find({ blogId: req.params.id }).populate("createdBy");
+    return res.render("blog", {
         user: req.user,
         blog: blog,
         comments: comments,
-    })
-})
+    });
+});
 
-router.post('/comment/:blogId', async(req,res)=>{
+router.post("/comment/:blogId", async (req, res) => {
     const comment = await Comment.create({
-        content: req.body.comment, 
+        content: req.body.comment,
         blogId: req.params.blogId,
         createdBy: req.user._id,
-    })
-    return res.redirect(`/blog/${req.params.blogId}`)
-})
+    });
+    return res.redirect(`/blog/${req.params.blogId}`);
+});
 
 router.post("/", upload.single("coverImage"), async (req, res) => {
     const { title, body } = req.body;
+    if (!req.file) {
+        return res.status(400).json({ message: "Image upload failed" });
+    }
     const blog = await Blog.create({
-      body,
-      title,
-      createdBy: req.user._id,
-      coverImageURL: `/uploads/${req.file.filename}`,
+        body,
+        title,
+        createdBy: req.user._id,
+        coverImageURL: req.file.path, // Cloudinary URL
     });
-    return res.redirect(`/blog/${blog._id}`); // Use backticks for string interpolation
-  });
+    return res.redirect(`/blog/${blog._id}`);
+});
 
 module.exports = router;
